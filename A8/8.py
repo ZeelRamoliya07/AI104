a44e6f17e49eb41b3b430e339e18d6b25e7bbecc
ZeelRamoliya07 <u24ai056@aid.svnit.ac.in>
2025-03-27 16:53:15 +0530

    Refined code

--- A8/8.py
+++ A8/8.py
@@ -1,91 +1,36 @@
-# Create a class for representing any 2-D point or vector. The methods inside this class include
-# its magnitude and its rotation with respect to the X-axis. Using the objects define functions for
-# calculating the distance between two vectors, dot product, cross product of two vectors. Extend
-# the 2-D vectors into 3-D using the concept of inheritance. Update the methods according to 3-
-# D.
+# Decode the message:
+# A message containing the letters from A-Z can be encoded into the numbers using the mapping
+# A-> 1, B-> 2, C-> 3, ..., Z-> 26. To decode an encoded message, you need to group the digits
+# and do the reverse mapping. You are required to display all the possible decoded messages.
+# For example: "11106" can be decoded into:
+# a. "AAJF" with the grouping (1 1 10 6)
+# b. "KJF" with the grouping (11 10 6)
 
-import math
+def decode_ways(self, index=0, current_decoding="", results=None):
+    if results is None:
+        results = []
 
-# Class representing a 2D vector
-class Vector2D:
-    def _init_(self, x, y):
-        self.x = x
-        self.y = y
-    
-    #magnitude of the vector
-    def magnitude(self):
-        return math.sqrt(self.x*2 + self.y*2)
-    
-    # angle with respect to the x-axis
-    def angle_with_x_axis(self):
-        return math.degrees(math.atan2(self.y, self.x))
-    
-    # distance between two vectors
-    def distance(self, other):
-        return math.sqrt((self.x - other.x)*2 + (self.y - other.y)*2)
-    
-    # dot product of two vectors
-    def dot_product(self, other):
-        return self.x * other.x + self.y * other.y
-    
-    # cross product of two vectors
-    def cross_product(self, other):
-        return self.x * other.y - self.y * other.x
+   
+    if index == len(self):
+        results.append(current_decoding)
+        return
 
-# Extending 2D vector into a 3D vector
-class Vector3D(Vector2D):
-    def _init_(self, x, y, z):
-        super()._init_(x, y)
-        self.z = z
     
-    #  magnitude
-    def magnitude(self):
-        return math.sqrt(self.x*2 + self.y2 + self.z*2)
+    num1 = int(self[index])
+    if 1 <= num1 <= 9:
+        decode_ways(self, index + 1, current_decoding + chr(num1 + 64), results)
+
     
-    # dot product of two 3D vectors
-    def dot_product(self, other):
-        return self.x * other.x + self.y * other.y + self.z * other.z
-    
-    # cross product of two 3D vectors
-    def cross_product(self, other):
-        return Vector3D(
-            self.y * other.z - self.z * other.y,
-            self.z * other.x - self.x * other.z,
-            self.x * other.y - self.y * other.x
-        )
+    if index + 1 < len(self):
+        num2 = int(self[index:index + 2])
+        if 10 <= num2 <= 26:
+            decode_ways(self, index + 2, current_decoding + chr(num2 + 64), results)
+
+    return results
 
 
-def main():
-    choice = int(input("Enter 2 for 2D vector, 3 for 3D vector: "))
-    
-    if choice == 2:
-        x1, y1 = map(int, input("Enter x and y for first vector: ").split())
-        x2, y2 = map(int, input("Enter x and y for second vector: ").split())
-        v1 = Vector2D(x1, y1)
-        v2 = Vector2D(x2, y2)
-    
-        print("Magnitude of v1:", v1.magnitude())
-        print("Magnitude of v2:", v2.magnitude())
-        print("Angle of v1 with x-axis:", v1.angle_with_x_axis())
-        print("Distance between v1 and v2:", v1.distance(v2))
-        print("Dot product of v1 and v2:", v1.dot_product(v2))
-        print("Cross product of v1 and v2:", v1.cross_product(v2))
-    
-    elif choice == 3:
-        x1, y1, z1 = map(int, input("Enter x, y, and z for first vector: ").split())
-        x2, y2, z2 = map(int, input("Enter x, y, and z for second vector: ").split())
-        v3 = Vector3D(x1, y1, z1)
-        v4 = Vector3D(x2, y2, z2)
-    
-        print("Magnitude of v3:", v3.magnitude())
-        print("Magnitude of v4:", v4.magnitude())
-        print("Dot product of v3 and v4:", v3.dot_product(v4))
-        cross_v3_v4 = v3.cross_product(v4)
-        print("Cross product of v3 and v4:", (cross_v3_v4.x, cross_v3_v4.y, cross_v3_v4.z))
-    
-    else:
-        print("Invalid choice!")
-
-# Run the program
-if __name__== "__main__":
-    main()
+encoded_message = input("Enter encoded message: ")
+decoded_messages = decode_ways(encoded_message)
+print("Possible Decodings:")
+for msg in decoded_messages:
+    print(msg)
